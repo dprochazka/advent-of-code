e1fb0d0e4d27726f57d7a4957392892c40b5b8dd
dapro <prochazka.daniel97@gmail.com>
2023-12-02 21:23:03 +0100

    dec01 trebuchet part two

--- 2023/01/test_trebuchet.py
+++ 2023/01/test_trebuchet.py
@@ -7,9 +7,17 @@ EXAMPLE = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
-"""
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+foursixtwoninevtzzgntnlg6oneightbxp"""
 
-EXPECTED_VALUES = [12, 38, 15, 77]
+
+EXPECTED_VALUES = [12, 38, 15, 77, 29, 83, 13, 24, 42, 14, 76, 48]
 
 
 @pytest.mark.parametrize(
--- 2023/01/trebuchet.py
+++ 2023/01/trebuchet.py
@@ -1,11 +1,49 @@
 import re
 
-def parse_line(line):
-    numbers = re.findall("[0-9]", line)
-    return int(numbers[0] + numbers[-1])
 
-def calibrate_trebuchet(input_file):    
+def parse_line(line):
+    digits_dict = {
+        "0": 0,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+    regex = "|".join(digits_dict.keys())
+
+    # forward search
+    first_digit = re.search(regex, line).group(0)
+    first_digit = digits_dict[first_digit]
+
+    # backward search
+    def reverse(string):
+        return string[::-1]
+
+    second_digit = re.search(reverse(regex), reverse(line)).group(0)
+    second_digit = digits_dict[reverse(second_digit)]
+
+    return 10 * first_digit + second_digit
+
+
+def calibrate_trebuchet(input_file):
     return sum((parse_line(line) for line in input_file))
+
 
 if __name__ == "__main__":
     with open("input") as input_file:
