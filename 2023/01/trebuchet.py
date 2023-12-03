0397c2b33549c64ccc0985677a45cdb573b277f9
dapro <prochazka.daniel97@gmail.com>
2023-12-03 09:14:44 +0100

    delete inputs

--- 2023/01/trebuchet.py
+++ 2023/01/trebuchet.py
@@ -46,5 +46,5 @@ def calibrate_trebuchet(input_file):
 
 
 if __name__ == "__main__":
-    with open("input") as input_file:
+    with open("input.txt") as input_file:
         print(calibrate_trebuchet(input_file))
