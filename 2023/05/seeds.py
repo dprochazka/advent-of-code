e5076ce43087de56961220d1ad1f97e4205a4257
dapro <prochazka.daniel97@gmail.com>
2023-12-05 23:23:47 +0100

    dec05 part2

--- 2023/05/seeds.py
+++ 2023/05/seeds.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import functools
+import itertools
 
 
 def parse_map(lines):
@@ -12,6 +13,29 @@ def parse_map(lines):
         return num
 
     return m
+
+
+def parse_inversed_map(lines):
+    maps = [[int(x) for x in line.split()] for line in lines[1:]]
+
+    def im(num):
+        for inp, out, ran in maps:
+            if inp <= num < inp + ran:
+                return out + (num - inp)
+        return num
+
+    return im
+
+
+def get_seed_detector(seed_line):
+    def seed_detector(seed):
+        """Check if a seed exists as defined in Part Two"""
+        for s, r in itertools.batched(seed_line, 2):
+            if s <= seed < s + r:
+                return True
+        return False
+
+    return seed_detector
 
 
 def parse_text_blocks(lines):
@@ -27,19 +51,32 @@ def parse_text_blocks(lines):
     return blocks
 
 
-def parse_input(lines):
+def parse_input(lines, map_parser):
     seeds = [int(x) for x in lines.pop(0).split(":")[1].split()]
     blocks = parse_text_blocks(lines[1:])
-    maps = [parse_map(x) for x in blocks]
+    maps = [map_parser(x) for x in blocks]
     return seeds, maps
 
 
-def find_locations(lines):
-    seeds, maps = parse_input(lines)
+def find_locations_part_one(lines):
+    seeds, maps = parse_input(lines, parse_map)
     return [functools.reduce(lambda x, y: y(x), maps, seed) for seed in seeds]
+
+
+def find_minimal_location_part_two(lines):
+    seeds, imaps = parse_input(lines, parse_inversed_map)
+    detect = get_seed_detector(seeds)
+    chained_imap = lambda l: functools.reduce(lambda x, y: y(x), reversed(imaps), l)
+
+    loc = 0
+    while True:
+        if detect(chained_imap(loc)):
+            return loc
+        loc += 1
 
 
 if __name__ == "__main__":
     with open("input.txt") as input_file:
         lines = [line.rstrip() for line in input_file]
-    print(min(find_locations(lines)))
+    # print(min(find_locations_part_one(lines)))
+    print(find_minimal_location_part_two(lines))
--- 2023/05/test_seeds.py
+++ 2023/05/test_seeds.py
@@ -1,4 +1,9 @@
-from seeds import parse_map, find_locations
+from seeds import (
+    parse_map,
+    parse_inversed_map,
+    find_locations_part_one,
+    find_minimal_location_part_two,
+)
 
 EXAMPLE = """seeds: 79 14 55 13
 
@@ -39,6 +44,8 @@ SEED_RESULTS = [82, 43, 86, 35]
 
 EXPECTED_MINIMAL_RESULT = 35
 
+EXPECTED_RESULT_PART_TWO = 46
+
 
 def test_parse_map():
     INPUT = """light-to-temperature map:
@@ -60,5 +67,29 @@ def test_parse_map():
     assert 101 == m(101)
 
 
+def test_parse_inversed_map():
+    INPUT = """light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13"""
+    m = parse_inversed_map(INPUT.splitlines())
+    assert m(1) == 1
+    assert m(37) == 37
+    assert m(45) == 77
+    assert m(46) == 78
+    assert m(50) == 82
+    assert m(67) == 99
+    assert m(68) == 64
+    assert m(80) == 76
+    assert m(81) == 45
+    assert m(99) == 63
+    assert m(100) == 100
+    assert m(101) == 101
+
+
 def test_find_locations():
-    assert find_locations(EXAMPLE.splitlines()) == SEED_RESULTS
+    assert find_locations_part_one(EXAMPLE.splitlines()) == SEED_RESULTS
+
+
+def test_find_minimal_location_part_two():
+    find_minimal_location_part_two(EXAMPLE.splitlines()) == EXPECTED_RESULT_PART_TWO
