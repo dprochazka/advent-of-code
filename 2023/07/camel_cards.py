6678f76ec5ea8b8a8c34cda7d4064f0847921acf
dapro <prochazka.daniel97@gmail.com>
2023-12-07 22:46:20 +0100

    dec07 part2

--- 2023/07/camel_cards.py
+++ 2023/07/camel_cards.py
@@ -1,14 +1,15 @@
-def evaluate_hand(hand):
-    combos_val = {
-        "five-of-a-kind": 6,
-        "four-of-a-kind": 5,
-        "full-house": 4,
-        "three-of-a-kind": 3,
-        "two-pair": 2,
-        "one-pair": 1,
-        "high-card": 0,
-    }
+COMBOS_VALUES = {
+    "five-of-a-kind": 6 * 13**5,
+    "four-of-a-kind": 5 * 13**5,
+    "full-house": 4 * 13**5,
+    "three-of-a-kind": 3 * 13**5,
+    "two-pair": 2 * 13**5,
+    "one-pair": 1 * 13**5,
+    "high-card": 0 * 13**5,
+}
 
+
+def evaluate_hand(hand):
     # each value is a digit in base-13
     faces_val = {
         **{str(x): x - 2 for x in range(2, 10)},
@@ -25,31 +26,66 @@ def evaluate_hand(hand):
     )
 
     if len(set(hand)) == len(hand):
-        return combos_val["high-card"] * 13**5 + value
+        return COMBOS_VALUES["high-card"] + value
     if len(set(hand)) == len(hand) - 1:
-        return combos_val["one-pair"] * 13**5 + value
+        return COMBOS_VALUES["one-pair"] + value
     if len(set(hand)) == 1:
-        return combos_val["five-of-a-kind"] * 13**5 + value
+        return COMBOS_VALUES["five-of-a-kind"] + value
 
     face_counts = [hand.count(ch) for ch in faces_val.keys()]
 
     if max(face_counts) == 4:
-        return combos_val["four-of-a-kind"] * 13**5 + value
+        return COMBOS_VALUES["four-of-a-kind"] + value
 
     face_counts = sorted(face_counts, reverse=True)
 
     if face_counts[0] == 3 and face_counts[1] == 2:
-        return combos_val["full-house"] * 13**5 + value
+        return COMBOS_VALUES["full-house"] + value
     elif face_counts[0] == 3:
-        return combos_val["three-of-a-kind"] * 13**5 + value
+        return COMBOS_VALUES["three-of-a-kind"] + value
     else:
-        return combos_val["two-pair"] * 13**5 + value
+        return COMBOS_VALUES["two-pair"] + value
 
 
-def evaluate_game(lines):
+def evaluate_hand_part_two(hand):
+    regular_faces = [
+        *[str(x) for x in range(2, 10)],
+        "T",
+        "Q",
+        "K",
+        "A",
+    ]
+    faces_val = {"J": 0, **{f: i + 1 for i, f in enumerate(regular_faces)}}
+
+    # value of cards only (without combos)
+    value = sum(
+        (faces_val[char] * 13**power for power, char in enumerate(reversed(hand)))
+    )
+
+    face_counts = [hand.count(ch) for ch in regular_faces]
+    face_counts = sorted(face_counts, reverse=True)
+    face_counts[0] += hand.count("J")
+
+    if face_counts[0] == 5:
+        return COMBOS_VALUES["five-of-a-kind"] + value
+    elif face_counts[0] == 4:
+        return COMBOS_VALUES["four-of-a-kind"] + value
+    if face_counts[0] == 3 and face_counts[1] == 2:
+        return COMBOS_VALUES["full-house"] + value
+    elif face_counts[0] == 3:
+        return COMBOS_VALUES["three-of-a-kind"] + value
+    elif face_counts[0] == 2 and face_counts[1] == 2:
+        return COMBOS_VALUES["two-pair"] + value
+    elif face_counts[0] == 2:
+        return COMBOS_VALUES["one-pair"] + value
+    else:
+        return value
+
+
+def evaluate_game(lines, hand_evaluator):
     parsed_lines = sorted(
         [l.split() for l in lines],  # tuples hand, bet
-        key=lambda x: evaluate_hand(x[0]),
+        key=lambda x: hand_evaluator(x[0]),
     )
     return sum((rank * int(line[1]) for rank, line in enumerate(parsed_lines, start=1)))
 
@@ -57,4 +93,4 @@ def evaluate_game(lines):
 if __name__ == "__main__":
     with open("input.txt") as input_file:
         lines = [line.rstrip() for line in input_file]
-    print(evaluate_game(lines))
+    print(evaluate_game(lines, evaluate_hand_part_two))
--- 2023/07/test_camel_cards.py
+++ 2023/07/test_camel_cards.py
@@ -1,14 +1,22 @@
-from camel_cards import evaluate_game
+from camel_cards import evaluate_game, evaluate_hand, evaluate_hand_part_two
 
 EXAMPLE = """32T3K 765
 T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483
-"""
+""".splitlines()
 
-EXPECTED_TOTAL_WINNINGS = 6440
+EXPECTED_TOTAL_WINNINGS_PART_ONE = 6440
+EXPECTED_TOTAL_WINNINGS_PART_TWO = 5905
 
 
-def test_evaluate_game():
-    assert evaluate_game(EXAMPLE.splitlines()) == EXPECTED_TOTAL_WINNINGS
+def test_evaluate_game_part_one():
+    assert evaluate_game(EXAMPLE, evaluate_hand) == EXPECTED_TOTAL_WINNINGS_PART_ONE
+
+
+def test_evaluate_game_part_two():
+    assert (
+        evaluate_game(EXAMPLE, evaluate_hand_part_two)
+        == EXPECTED_TOTAL_WINNINGS_PART_TWO
+    )
