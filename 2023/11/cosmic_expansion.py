6d599db04cdda7dc2fced0da05dbc51b7d23a5e8
dapro <prochazka.daniel97@gmail.com>
2023-12-11 20:47:13 +0100

    dec11 part2

--- 2023/11/cosmic_expansion.py
+++ 2023/11/cosmic_expansion.py
@@ -1,5 +1,5 @@
 import itertools
-# import math
+
 
 def parse_universe(lines):
     empty_horizontal = set(range(len(lines)))
@@ -13,16 +13,23 @@ def parse_universe(lines):
                 empty_vertical = empty_vertical - {j}
     return galaxies, empty_horizontal, empty_vertical
 
-def measure_universe(lines):
+
+def measure_universe(lines, factor):
     galaxies, eh, ev = parse_universe(lines)
     total_distance = 0
     for g1, g2 in itertools.combinations(galaxies, 2):
-        vertical_distance = abs(g1[0] - g2[0]) + len(set(range(min(g1[0], g2[0]), max(g1[0], g2[0]))) & eh)
-        horizontal_distance = abs(g1[1] - g2[1]) + len(set(range(min(g1[1], g2[1]), max(g1[1], g2[1]))) & ev)
-        total_distance += (horizontal_distance + vertical_distance)
+        vertical_distance = abs(g1[0] - g2[0]) + (factor - 1) * len(
+            set(range(min(g1[0], g2[0]), max(g1[0], g2[0]))) & eh
+        )
+        horizontal_distance = abs(g1[1] - g2[1]) + (factor - 1) * len(
+            set(range(min(g1[1], g2[1]), max(g1[1], g2[1]))) & ev
+        )
+        total_distance += horizontal_distance + vertical_distance
     return total_distance
+
 
 if __name__ == "__main__":
     with open("input.txt") as input_file:
         lines = [line.rstrip() for line in input_file]
-    print(measure_universe(lines))
+    print(measure_universe(lines, 2))
+    print(measure_universe(lines, 10**6))
