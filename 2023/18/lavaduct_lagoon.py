bb0189b2ac96a9ad2afc0310786f9cbd1a938712
dapro <prochazka.daniel97@gmail.com>
2023-12-19 00:22:54 +0100

    dec18 part2

--- 2023/18/lavaduct_lagoon.py
+++ 2023/18/lavaduct_lagoon.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum
-import matplotlib.pyplot as plt
+import itertools
+
 
 class Direction(Enum):
     R = "R"
@@ -10,7 +11,7 @@ class Direction(Enum):
 
     def coord(self):
         if self == Direction.R:
-            return (1,0)
+            return (1, 0)
         elif self == Direction.L:
             return (-1, 0)
         elif self == Direction.U:
@@ -18,78 +19,88 @@ class Direction(Enum):
         elif self == Direction.D:
             return (0, -1)
 
+    @classmethod
+    def from_num(cls, num):
+        return [Direction.R, Direction.D, Direction.L, Direction.U][num]
+
+
 @dataclass(frozen=True)
 class Instruction:
     d: Direction
     l: int
-    c: str
 
     @classmethod
     def from_string(cls, s):
+        # Part One
         d, l, c = s.split()
-        return cls(Direction(d), int(l), c.strip("()"))
+        return cls(Direction(d), int(l))
 
-def parse_input(lines):
-    return [Instruction.from_string(l) for l in lines]
+    @classmethod
+    def from_hex(cls, s):
+        # Part Two
+        _, h = s.strip(")").split("#")
+        return cls(Direction.from_num(int(h[-1])), int(h[:-1], base=16))
 
-def measure_lagoon(lines):
-    instructions = parse_input(lines)
+
+def _measure_lagoon(instructions):
     path = [(0, 0)]
-    for i in instructions:
-        for _ in range(i.l):
-            x0, y0 = path[-1]
-            dx, dy = i.d.coord()
-            path.append((x0 + dx, y0 + dy))
+    corners_left = []
+    corners_right = []
+    for i1, i2 in itertools.pairwise([*instructions, instructions[0], instructions[1]]):
+        x0, y0 = path[-1]
+        dx, dy = i1.d.coord()
+        x, y = x0 + i1.l * dx, y0 + i1.l * dy
+        path.append((x, y))
 
-    x_min = min(p[0] for p in path)
-    y_min = min(p[1] for p in path)
-    x_max = max(p[0] for p in path)
-    y_max = max(p[1] for p in path)
+        # TODO generalize this with coord() some day
+        if i1.d == Direction.U and i2.d == Direction.L:
+            corners_left.append((x - 0.5, y - 0.5))  # inner turn
+            corners_right.append((x + 0.5, y + 0.5))  # outer turn
+        elif i1.d == Direction.U and i2.d == Direction.R:
+            corners_left.append((x - 0.5, y + 0.5))  # outer turn
+            corners_right.append((x + 0.5, y - 0.5))  # inner turn
+        elif i1.d == Direction.D and i2.d == Direction.R:
+            corners_left.append((x + 0.5, y + 0.5))
+            corners_right.append((x - 0.5, y - 0.5))
+        elif i1.d == Direction.D and i2.d == Direction.L:
+            corners_left.append((x + 0.5, y - 0.5))
+            corners_right.append((x - 0.5, y + 0.5))
+        elif i1.d == Direction.L and i2.d == Direction.U:
+            corners_left.append((x - 0.5, y - 0.5))
+            corners_right.append((x + 0.5, y + 0.5))
+        elif i1.d == Direction.L and i2.d == Direction.D:
+            corners_left.append((x + 0.5, y - 0.5))
+            corners_right.append((x - 0.5, y + 0.5))
+        elif i1.d == Direction.R and i2.d == Direction.D:
+            corners_left.append((x + 0.5, y + 0.5))
+            corners_right.append((x - 0.5, y - 0.5))
+        elif i1.d == Direction.R and i2.d == Direction.U:
+            corners_left.append((x - 0.5, y + 0.5))
+            corners_right.append((x + 0.5, y - 0.5))
 
-    path = set(path)
-    inside_vol = 0
-    for y in range(y_min, y_max+1):
-        inside = False
-        segment_start = None
-        for x in range(x_min, x_max+1):
-            if {(x, y), (x, y-1), (x, y+1)} <= path:
-                # vertical segment
-                inside = not inside
-            elif {(x,y), (x-1, y), (x+1, y)} <= path:
-                # horizontal segment - no change
-                pass
-            elif {(x,y), (x+1, y), (x, y+1)} <= path:
-                # L segment
-                segment_start = "L"
-            elif {(x,y), (x+1, y), (x, y-1)} <= path:
-                # F segment
-                segment_start = "F"
-            elif {(x,y), (x-1, y), (x, y+1)} <= path:
-                # J segment
-                if segment_start == "F":
-                    inside = not inside
-                segment_start = None
-            elif {(x,y), (x-1, y), (x, y-1)} <= path:
-                # 7 segment
-                if segment_start == "L":
-                    inside = not inside
-                segment_start = None
-            if inside and (x,y) not in path:
-                inside_vol += 1
-    return inside_vol + len(path)
+    def volume_using_trapezoids(nodes):
+        volume = 0
+        for (x1, y1), (x2, y2) in itertools.pairwise([*nodes, nodes[0]]):
+            volume += 0.5 * (y1 + y2) * (x1 - x2)
+        return abs(volume)
 
-def visualize_lagoon(lines):
-    instructions = parse_input(lines)
-    path = [(0, 0)]
-    for i in instructions:
-        for _ in range(i.l):
-            x0, y0 = path[-1]
-            dx, dy = i.d.coord()
-            path.append((x0 + dx, y0 + dy))
-    plt.plot([p[0] for p in path], [p[1] for p in path])
-    plt.show()
+    v1 = volume_using_trapezoids(corners_left)
+    v2 = volume_using_trapezoids(corners_right)
+    # whichever of these is smaller is the inner volume only
+    # the other one includes the original digger's path
+    return int(max(v1, v2))
+
+
+def measure_lagoon(part, lines):
+    if part == 1:
+        instructions = [Instruction.from_string(l) for l in lines]
+    elif part == 2:
+        instructions = [Instruction.from_hex(l) for l in lines]
+    return _measure_lagoon(instructions)
+
 
 if __name__ == "__main__":
     with open("input.txt") as input_file:
         lines = [line.rstrip() for line in input_file]
-    print(measure_lagoon(lines))
+    # print(measure_lagoon_pipemaze(lines))
+    print(measure_lagoon(2, lines))
--- 2023/18/test_lavaduct_lagoon.py
+++
@@ -1,9 +0,0 @@
-from lavaduct_lagoon import measure_lagoon
-
-EXAMPLE = """
-""".splitlines()
-
-EXPECTED = None
-
-def test_measure_lagoon():
-    assert measure_lagoon(EXAMPLE) == EXPECTED
