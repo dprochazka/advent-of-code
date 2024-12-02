40bc4a218ee765c03693e517dc95be9172e243cd
dapro <prochazka.daniel97@gmail.com>
2024-12-03 00:03:14 +0100

    2024 dec02

---
+++ 2024/02/red_nosed_reports.py
@@ -0,0 +1,23 @@
+with open("input.txt") as input_file:
+    lines = [list(map(int, line.split())) for line in input_file]
+
+import itertools
+
+#### Part1 ####
+def is_safe(report):
+    inc = report[0] <= report[1]
+    for a, b in itertools.pairwise(report):
+        if not 1 <= abs(a - b) <= 3:
+            return False
+        if (inc and a > b) or (not inc and a < b):
+            return False
+    return True
+
+print(sum(map(is_safe, lines)))
+
+
+#### Part2 ####
+def is_safe_p2_bruteforce(report):
+    return any(map(is_safe, itertools.combinations(report, len(report) - 1)))
+
+print(sum(map(is_safe_p2_bruteforce, lines)))
